529e8c83400c441510d71353d2b282fdc598ee94
javezjackson376-code <javezjackson376@gmail.com>
2026-04-28 23:00:47 -0400

    Update ds.lua

--- ds.lua
+++ ds.lua
@@ -1,14 +1,17 @@
--- MAC DESYNC SCRIPT
 local RunService = game:GetService("RunService")
 local LP = game.Players.LocalPlayer
 
+-- Use Heartbeat for maximum priority
 RunService.Heartbeat:Connect(function()
     local Char = LP.Character
-    if Char and Char:FindFirstChild("HumanoidRootPart") then
-        local oldV = Char.HumanoidRootPart.Velocity
-        Char.HumanoidRootPart.Velocity = Vector3.new(0, -100, 0)
+    local Root = Char and Char:FindFirstChild("HumanoidRootPart")
+    
+    if Root then
+        -- Forcefully sets velocity and CFrame offset
+        local oldV = Root.Velocity
+        Root.Velocity = Vector3.new(0, 5000, 0) -- High vertical velocity
+        Root.CFrame = Root.CFrame * CFrame.new(0, 0.0001, 0)
         RunService.RenderStepped:Wait()
-        Char.HumanoidRootPart.Velocity = oldV
+        Root.Velocity = oldV
     end
 end)
-print("Desync Loaded")
